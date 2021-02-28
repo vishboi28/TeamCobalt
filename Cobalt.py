49b6dab4629ea80e507041331f2f42675a391a46
NobleTempest <79363596+NobleTempest@users.noreply.github.com>
2021-02-28 09:23:58 +0530

    Update Cobalt.py

--- Cobalt.py
+++ Cobalt.py
@@ -17,27 +17,36 @@ while option != 7:
     option = int(input("Please enter the required workout: (1 - 6): "))
 
     if(option == 1):
+        starter()
         stomach_crunch()
         oblique_crunch()
         plank()
         side_plank()
         stomach_crunch_with_legs_raised()
+        finisher()
     elif (option == 2):
+        starter()
         press_up()
         close_grip_wall_push_ups()
         bench_dips()
         tricep_kickbacks()
+        finisher()
     elif (option == 3):
+        starter()
         side_lying_leg_raise()
         bridges()
         one_leg_kickbacks()
         lunges()
+        finisher()
     elif (option == 4):
+        starter()
         rocket_jumps()
         squats()
         tap_backs()
         burpees()
+        finisher()
     elif (option == 5):
+        starter()
         squats()
         lunges()
         calf_raises()
@@ -45,6 +54,7 @@ while option != 7:
         stomach_crunch()
         oblique_crunch()
         back_raises()
+        finisher()
     elif(option==6):
         print("End of Workout, Thank you")
         time.sleep(2)
